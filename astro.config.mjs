8278451d7fc8868f6bef4ca197f86d9f8cb5554c
Oscar Elías <oscarmateoelias@gmail.com>
2024-03-27 13:35:02 -0600

    Finalización de Tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astrotuto-mt.netlify.app/"
+  site: "https://astrotuto-mt.netlify.app/",
+  integrations: [preact()]
 });
